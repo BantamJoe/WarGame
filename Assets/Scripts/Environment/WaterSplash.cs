3e572ea98a6f02a50bcc9267b73b3dd74df877ef
kucab2345 <kucab2345@gmail.com>
2018-06-18 23:04:25 -0500

    Artillery spawning testing

---
+++ Assets/Scripts/Environment/WaterSplash.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSplash : MonoBehaviour {
+
+    public GameObject waterSplashEffect;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.layer == 1 << 8)
+            Destroy(Instantiate(waterSplashEffect, other.transform.position, Quaternion.LookRotation(Vector3.up)), 1f);
+    }
+}
---
+++ Assets/Scripts/Weapon/ArtillerySpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtillerySpawner : MonoBehaviour {
+
+    public GameObject artilleryShell;
+    public int repeat = 1;
+    public float delay = 3f;
+    public float initialDelay = 1f;
+    public float delayRandom = 1f;
+    public float positionRandom = 5f;
+
+    private float counter = 0;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("Projectile", initialDelay, delay + Random.Range(-delayRandom, delayRandom));
+    }
+    void Projectile()
+    {
+        if(counter < repeat)
+        {
+            counter += 1;
+            Vector3 directionOfShot = transform.forward;
+            directionOfShot.x += Random.Range(-positionRandom, positionRandom);
+            directionOfShot.z += Random.Range(-positionRandom, positionRandom);
+
+            Instantiate(artilleryShell, transform.position + directionOfShot, Quaternion.LookRotation(Vector3.down));
+        }
+    }
+}
