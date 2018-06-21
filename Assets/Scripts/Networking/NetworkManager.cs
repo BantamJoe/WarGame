a459e39f4bebc5714d01f59c5daef4db2b61b768
packerbacker8 <chandler@gbpackerfans.com>
2018-06-20 23:36:59 -0600

    Start of networking tasks

---
+++ Assets/Scripts/Networking/NetworkManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    public static string gameVersion = "0.0.5";
+    public Camera worldCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+        Connect();
+    }
+
+    /// <summary>
+    /// Connects to the server using the settings set up in the Photon
+    /// Server Settings object.
+    /// </summary>
+    private void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings("v" + gameVersion);
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    private void OnJoinedLobby()
+    {
+        Debug.Log("Attempting to join a room...");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    private void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("Failed to join random room!");
+        Debug.Log("Creating new room instead...");
+        PhotonNetwork.CreateRoom("Arena1");
+    }
+
+    private void OnJoinedRoom()
+    {
+        Debug.Log("Joined the room!");
+
+        SpawnAPlayer();
+    }
+
+    public void SpawnAPlayer()
+    {
+        //disables world camera
+        worldCamera.enabled = false;
+        //prefabs must be in a resource folder
+        PhotonNetwork.Instantiate("BritishRifleman", Vector3.zero, Quaternion.identity, 0);
+
+    }
+}
